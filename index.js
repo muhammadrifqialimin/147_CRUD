1c90f1147d2a13cb6bbed0d41ceb8a26ad57e64d
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-22 11:58:12 +0700

    menambahkan connection database

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const app = express();
 const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  port: "3309",
+  password: "Rifqy2004_",
+  database: "mahasiswa",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to database.");
+});
