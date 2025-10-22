5c3e53a33dc063d240aa4f933d0ba04fe63e31e1
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-10-22 11:56:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
